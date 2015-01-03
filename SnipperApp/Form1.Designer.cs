757fcd9a70580c94baa734f22d629aa5df7be461
SaruchiA <arora.saruchi@gmail.com>
2015-01-02 16:01:21 -0800

    Added main project

---
+++ SnipperApp/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 102);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(486, 142);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 267);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(486, 139);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(526, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Snip";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Specify Cutoff Value";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(156, 13);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 418);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        //private void textBox3_TextChanged(object sender, System.EventArgs e)
+        //{
+        //    throw new System.NotImplementedException();
+        //}
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ SnipperApp/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        string str;
+        //static string str = "India officially Iambigword the Republic of India is a country in South Asiau.";
+
+        char[] s;
+        char[] specialChar = "()-\"\\//.',;? ".ToCharArray();
+        protected int cutOff=20;
+        string result;
+      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             this.ActiveControl = textBox3;
+        }
+
+        public void showSnippedString()
+        {
+            textBox2.Text =  result+"...";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            str = textBox1.Text;
+            if (string.IsNullOrEmpty(textBox3.Text) || string.IsNullOrWhiteSpace(textBox3.Text) || (textBox3 .Text).All(char.IsLetter ) )
+            {
+                result = " ";
+                showSnippedString();
+
+            }
+            else if (!string.IsNullOrEmpty(textBox3.Text) || !string.IsNullOrWhiteSpace(textBox3.Text))
+            {
+                {
+                    cutOff = int.Parse(textBox3.Text);
+                }
+                if (cutOff > str.Length || cutOff == 0)
+                {
+                    if (cutOff > str.Length)
+                    {
+                        result = str;
+                        showSnippedString();
+                    }
+
+                    else
+                    {
+                        result = "";
+                        showSnippedString();
+                    }
+                }
+
+                else if (cutOff == str.Length || cutOff < str.Length)
+                {
+                    if (cutOff == str.Length)
+                    {
+                        s = str.Substring(0, cutOff).ToCharArray();
+                        result = new string(s);
+                       // showSnippedString();
+                        
+                    }
+
+                    else if (cutOff < str.Length)
+                    {
+                        s = str.Substring(0, cutOff + 1).ToCharArray();
+                    
+                    if (!isSpecial(s[cutOff - 1]))
+                    {
+                        if (isSpecial(s[cutOff]))
+                        {
+                            result = new string(s);
+                        }
+                        else
+                        {
+                            for (int i = (cutOff); i > 0; i--)
+                            {
+                                if (s[i] == ' ')
+                                {
+                                    int d = i;
+                                    result = (new string(s)).Substring(0, d);
+                                    break;
+                                }
+                                else
+                                {
+                                    result = (new string(s)).Substring (0,(s.Length -1));
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    else
+                        result = new string(s);
+                    }
+                }
+
+                else
+                {
+                    result = new string(s);
+                }
+
+                showSnippedString();
+
+
+
+            }
+        }
+
+        private bool isSpecial(char c)
+        {
+            return specialChar.Contains(c);
+        }
+
+       
+
+    }
+}
