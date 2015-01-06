f9f9a50c6ec26960b2ae9c7494ae4e2be3b0cb51
SaruchiA <arora.saruchi@gmail.com>
2015-01-05 17:16:24 -0800

    Fully functional multi-line snipper

--- SnipperApp/Form1.cs
+++ SnipperApp/Form1.cs
@@ -12,14 +12,14 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
-        string str;
-        //static string str = "India officially Iambigword the Republic of India is a country in South Asiau.";
-
-        char[] s;
+        #region Properties
+        String str;
+        
         char[] specialChar = "()-\"\\//.',;? ".ToCharArray();
-        protected int cutOff=20;
-        string result;
-      
+        int cutOff;
+        #endregion
+        
+        #region Event Handlers
         public Form1()
         {
             InitializeComponent();
@@ -27,121 +27,108 @@ namespace WindowsFormsApplication1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-             this.ActiveControl = textBox3;
-        }
-
-        public void showSnippedString()
-        {
-            textBox2.Text =  result+"...";
+             this.ActiveControl = textBox1;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             str = textBox1.Text;
-            if (string.IsNullOrEmpty(textBox3.Text) || string.IsNullOrWhiteSpace(textBox3.Text) || (textBox3 .Text).All(char.IsLetter ) )
+            String result="";
+            
+            List<String> linesOfText = new List<string>();
+            linesOfText.AddRange(str.Split(new string[] { "\n", "\r\n", "\r" }, StringSplitOptions.RemoveEmptyEntries));
+           
+            /* BeginSection:-returing result if cutoff is some invalid value*/
+            if (string.IsNullOrEmpty(textBox3 .Text ) || string.IsNullOrWhiteSpace(textBox3 .Text ) || (textBox3 .Text ).All(char.IsLetter))
             {
-                result = " ";
-                showSnippedString();
+                result = "";
+                showSnippedString(result);
 
             }
-            else if (!string.IsNullOrEmpty(textBox3.Text) || !string.IsNullOrWhiteSpace(textBox3.Text))
+            else
             {
+                cutOff = int.Parse(textBox3.Text);
+                Manipulator m1 = new Manipulator();
+                for (int i = 0; i < linesOfText.Count(); i++)
                 {
-                    cutOff = int.Parse(textBox3.Text);
+                    result += m1.Snip(linesOfText[i], cutOff)+"..."+Environment.NewLine;
                 }
-                if (cutOff > str.Length || cutOff == 0)
-                {
-                    if (cutOff > str.Length)
-                    {
-                        result = str;
-                        showSnippedString();
-                    }
 
+            }
+            showSnippedString(result);
+            /* EndSection:-returing result if cutoff is some invalid value*/ 
+
+           
+            
+                         
+        }
+        #endregion
+
+        public  void showSnippedString(string result)
+        {
+         //   MessageBox.Show(result+"...");
+            textBox2.Lines = result.Split(new string[]{"\n"},StringSplitOptions.None);
+        }
+    } // end Form1
+    
+        public class Manipulator
+        {   
+            private char[] specialChar = "()-\"\\//.',;? ".ToCharArray();
+            public String result;
+            private bool isSpecial(char c)
+            {
+                return specialChar.Contains(c);
+            }
+            
+            public string Snip(string input, int cutoffVal)
+            {
+
+                if (cutoffVal > input.Length || cutoffVal == 0)
+                {
+                    if (cutoffVal > input.Length)
+                    {
+                        return input;
+                    }
                     else
                     {
-                        result = "";
-                        showSnippedString();
-                    }
-                }
-
-                else if (cutOff == str.Length || cutOff < str.Length)
-                {
-                    if (cutOff == str.Length)
-                    {
-                        s = str.Substring(0, cutOff).ToCharArray();
-                        result = new string(s);
-                       // showSnippedString();
-                        
+                        return string.Empty;
                     }
 
-                    else if (cutOff < str.Length)
-                    {
-                        s = str.Substring(0, cutOff + 1).ToCharArray();
-
-                        if (!isSpecial(s[cutOff - 1]))
-                        {
-                            if (isSpecial(s[cutOff]))
-                            {
-                                result = new string(s);
-                            }
-                            else
-                            {
-                                for (int i = (cutOff); i > 0; i--)
-                                {
-                                    if (s[i] == ' ')
-                                    {
-                                        int d = i;
-                                        result = (new string(s)).Substring(0, d);
-                                        break;
-                                    }
-                                    else
-                                    {
-                                        result = (new string(s)).Substring(0, (s.Length - 1));
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                        else
-                        {
-                            result = new string(s);
-                            for (int i = (cutOff); i > 0; i--)
-                            {
-                                if (s[i] == ' ')
-                                {
-                                    int d = i;
-                                    result = (new string(s)).Substring(0, d);
-                                    break;
-                                }
-                                else
-                                {
-                                    result = (new string(s)).Substring(0, (s.Length - 1));
-                                    break;
-                                }
-                            }
-
-                        }
-                    }
                 }
 
                 else
                 {
-                    result = new string(s);
+                    result = input.Substring(0, cutoffVal);
+                        
+                        if ((cutoffVal+1) <= input.Length)
+                        {
+                            result = input.Substring(0, cutoffVal + 1);
+                            if (!isSpecial(result[cutoffVal - 1]))
+                            {
+                                if (isSpecial(input[cutoffVal]))
+                                {
+                                    result = input;
+                                }
+                                else
+                                {
+                                    for (int i = (cutoffVal); i > 0; i--)
+                                    {
+                                        if (input[i] == ' ')
+                                        {
+                                            int d = i;
+                                            result = input.Substring(0, d);
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    return result;
                 }
-
-                showSnippedString();
-
-
-
             }
-        }
+        } // end class Manipulator
 
-        private bool isSpecial(char c)
-        {
-            return specialChar.Contains(c);
-        }
-
-       
-
-    }
+        
+        
+   
 }
